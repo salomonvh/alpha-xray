aba02291979b05dc28221e88b47822c0afd30cf0
Herman <hfbmartinus@gmail.com>
2018-08-06 14:21:05 +0200

    Initialising repo

---
+++ Assets/ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ResetPosition () {
+		transform.position = new Vector3(0, 1.2f, 0);
+		var rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = Vector3.zero;
+    rigidbody.angularVelocity = Vector3.zero;
+	}
+}
---
+++ Assets/root.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class root : MonoBehaviour {
+
+	public string api_key = "";
+
+	public void CreateQuote() {
+      StartCoroutine(CreateQuoteCoroutine());
+  }
+
+  IEnumerator CreateQuoteCoroutine() {
+      // List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+      // formData.Add( new MultipartFormDataSection("username=&password=''") );
+			//
+      // UnityWebRequest www = UnityWebRequest.Post("https://api.root.co.za/v1/insurance", formData);
+      // yield return www.SendWebRequest();
+			//
+      // if(www.isNetworkError || www.isHttpError) {
+      //     Debug.Log(www.error);
+      // }
+      // else {
+      //     Debug.Log("Form upload complete!");
+      // }
+			Debug.Log("Quote created");
+			yield return true;
+  }
+}
