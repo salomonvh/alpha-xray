364e34cf7831038082a04314edeb1c90dec80423
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-08-06 16:44:31 +0200

    Add root.cs script, still WIP

--- Assets/root.cs
+++ Assets/root.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -12,18 +13,25 @@ public class root : MonoBehaviour {
   }
 
   IEnumerator CreateQuoteCoroutine() {
-      // List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-      // formData.Add( new MultipartFormDataSection("username=&password=''") );
-			//
-      // UnityWebRequest www = UnityWebRequest.Post("https://api.root.co.za/v1/insurance", formData);
-      // yield return www.SendWebRequest();
-			//
-      // if(www.isNetworkError || www.isHttpError) {
-      //     Debug.Log(www.error);
-      // }
-      // else {
-      //     Debug.Log("Form upload complete!");
-      // }
+      List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+
+      string auth = api_key + ":";
+      auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+      auth = "Basic " + auth;
+
+      formData.Add( new MultipartFormDataSection("type=root_gadgets") );
+      formData.Add( new MultipartFormDataSection("model_name=iPhone 6s 64GB LTE") );
+	     
+      UnityWebRequest www = UnityWebRequest.Post("https://sandbox.root.co.za/v1/insurance/quotes", formData);
+      www.SetRequestHeader("AUTHORIZATION", auth);
+      yield return www.SendWebRequest();
+	     
+      if(www.isNetworkError || www.isHttpError) {
+          Debug.Log(www.error);
+      }
+      else {
+          Debug.Log("Form upload complete!");
+      }
 			Debug.Log("Quote created");
 			yield return true;
   }
