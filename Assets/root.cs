daab6ad911ce9e6c344e9dd59744981aa1281434
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-08-15 15:26:09 +0200

    Add working root.cs script for getting a Root Quote

--- Assets/root.cs
+++ Assets/root.cs
@@ -4,35 +4,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class root : MonoBehaviour {
 
-	public string api_key = "";
+public class root : MonoBehaviour
+{
 
-	public void CreateQuote() {
-      StartCoroutine(CreateQuoteCoroutine());
-  }
+    /* Create a Root Object to store the returned json data in */
+    [System.Serializable]
+    public class Quotes
+    {
+        public Quote[] values;
+    }
 
-  IEnumerator CreateQuoteCoroutine() {
-      List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+    [System.Serializable]
+    public class Quote
+    {
+        public string package_name;
+        public string sum_assured;
+        public int base_premium;
+        public string suggested_premium;
+        public string created_at;
+        public string quote_package_id;
+        public QuoteModule module;
+    }
 
-      string auth = api_key + ":";
-      auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
-      auth = "Basic " + auth;
+    [System.Serializable]
+    public class QuoteModule
+    {
+        public string type;
+        public string make;
+        public string model;
+    }
 
-      formData.Add( new MultipartFormDataSection("type=root_gadgets") );
-      formData.Add( new MultipartFormDataSection("model_name=iPhone 6s 64GB LTE") );
-	     
-      UnityWebRequest www = UnityWebRequest.Post("https://sandbox.root.co.za/v1/insurance/quotes", formData);
-      www.SetRequestHeader("AUTHORIZATION", auth);
-      yield return www.SendWebRequest();
-	     
-      if(www.isNetworkError || www.isHttpError) {
-          Debug.Log(www.error);
-      }
-      else {
-          Debug.Log("Form upload complete!");
-      }
-			Debug.Log("Quote created");
-			yield return true;
-  }
+    [Serializable]
+    public struct Param
+    {
+        public string key;
+        public string value;
+    }
+
+    public string api_key = "";
+
+    public void CallApi()
+    {
+        StartCoroutine(CreateQuoteCoroutine("iPhone 6S 64GB LTE"));
+    }
+
+	private void Start()
+	{
+        CallApi();
+	}
+
+    IEnumerator CreateQuoteCoroutine(String modelNr)
+    {
+
+        string auth = api_key + ":";
+        auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+        auth = "Basic " + auth;
+
+        WWWForm form = new WWWForm();
+        form.AddField("type", "root_gadgets");
+        form.AddField("model_name", modelNr);
+
+        UnityWebRequest www = UnityWebRequest.Post("https://sandbox.root.co.za/v1/insurance/quotes", form);
+        www.SetRequestHeader("AUTHORIZATION", auth);
+        yield return www.Send();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+           Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Quotes json = JsonUtility.FromJson<Quotes>("{\"values\":" + www.downloadHandler.text + "}");
+
+            String text = "Make: " + json.values[0].module.make + "\nPremium: R" + (json.values[0].base_premium / 100);
+            Debug.Log(modelNr + ": " + json.values[0].module.make);
+            Debug.Log(text);
+            Debug.Log("Form upload complete!");
+        }
+        yield return true;
+    }
 }
