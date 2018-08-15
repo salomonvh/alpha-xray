3f90e6951597eb71fa773aa6babed74fdb46a837
Herman <hfbmartinus@gmail.com>
2018-08-15 19:47:59 +0200

    Wooooop! Made the Root script more generic (and specific) without testing it and it just worked on the first run!

--- Assets/root.cs
+++ Assets/root.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,81 +9,87 @@ using UnityEngine.UI;
 public class root : MonoBehaviour
 {
 
-    /* Create a Root Object to store the returned json data in */
-    [System.Serializable]
-    public class Quotes
-    {
-        public Quote[] values;
+  /* Create a Root Object to store the returned json data in */
+  [System.Serializable]
+  public class Quotes
+  {
+    public Quote[] values;
+  }
+
+  [System.Serializable]
+  public class Quote
+  {
+    public string package_name;
+    public string sum_assured;
+    public int base_premium;
+    public string suggested_premium;
+    public string created_at;
+    public string quote_package_id;
+    public QuoteModule module;
+  }
+
+  [System.Serializable]
+  public class QuoteModule
+  {
+    public string type;
+    public string make;
+    public string model;
+  }
+
+  [Serializable]
+  public class Param
+  {
+    public Param (string _key, string _value) {
+      key = _key;
+      value = _value;
     }
+    public string key;
+    public string value;
+  }
 
-    [System.Serializable]
-    public class Quote
+  public string api_key = "";
+  public TextMesh canvas_text;
+
+  private void Start()
+  {
+    // CreateQuote("iPhone 6S 64GB LTE");
+  }
+
+  public void CreateQuote(string gadget) {
+    List<Param> parameters = new List<Param>();
+    parameters.Add(new Param("type", "root_gadgets"));
+    parameters.Add(new Param("model_name", gadget));
+
+    StartCoroutine(CallAPICoroutine("https://sandbox.root.co.za/v1/insurance/quotes", parameters));
+  }
+
+  IEnumerator CallAPICoroutine(String url, List<Param> parameters)
+  {
+
+    string auth = api_key + ":";
+    auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+    auth = "Basic " + auth;
+
+    WWWForm form = new WWWForm();
+    form.AddField(parameters[0].key, parameters[0].value);
+    form.AddField(parameters[1].key, parameters[1].value);
+
+    UnityWebRequest www = UnityWebRequest.Post(url, form);
+    www.SetRequestHeader("AUTHORIZATION", auth);
+    yield return www.Send();
+
+    if (www.isNetworkError || www.isHttpError)
     {
-        public string package_name;
-        public string sum_assured;
-        public int base_premium;
-        public string suggested_premium;
-        public string created_at;
-        public string quote_package_id;
-        public QuoteModule module;
+      Debug.Log(www.downloadHandler.text);
     }
-
-    [System.Serializable]
-    public class QuoteModule
+    else
     {
-        public string type;
-        public string make;
-        public string model;
+      Quotes json = JsonUtility.FromJson<Quotes>("{\"values\":" + www.downloadHandler.text + "}");
+
+      String text = "Make: " + json.values[0].module.make + "\nPremium: R" + (json.values[0].base_premium / 100);
+      Debug.Log("Form upload complete!");
+      Debug.Log(text);
     }
-
-    [Serializable]
-    public struct Param
-    {
-        public string key;
-        public string value;
-    }
-
-    public string api_key = "";
-    public Text canvas_text;
-
-    public void CallApi()
-    {
-        StartCoroutine(CreateQuoteCoroutine("iPhone 6S 64GB LTE"));
-    }
-
-	private void Start()
-	{
-        CallApi();
-	}
-
-    IEnumerator CreateQuoteCoroutine(String modelNr)
-    {
-
-        string auth = api_key + ":";
-        auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
-        auth = "Basic " + auth;
-
-        WWWForm form = new WWWForm();
-        form.AddField("type", "root_gadgets");
-        form.AddField("model_name", modelNr);
-
-        UnityWebRequest www = UnityWebRequest.Post("https://sandbox.root.co.za/v1/insurance/quotes", form);
-        www.SetRequestHeader("AUTHORIZATION", auth);
-        yield return www.Send();
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-           Debug.Log(www.downloadHandler.text);
-        }
-        else
-        {
-            Quotes json = JsonUtility.FromJson<Quotes>("{\"values\":" + www.downloadHandler.text + "}");
-
-            String text = "Make: " + json.values[0].module.make + "\nPremium: R" + (json.values[0].base_premium / 100);
-            Debug.Log(modelNr + ": " + json.values[0].module.make);
-            Debug.Log("Form upload complete!");
-            canvas_text.text = text; 
-        }
-        yield return true;
-    }
+    yield return true;
+  }
 }
