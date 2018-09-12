0a910d9c42466e971a4529d615fee062eaa12c86
Salomon <salomon@byteorbit.com>
2018-09-12 23:04:37 +0200

    Broken AR with added assets.

---
+++ Assets/QRBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QRBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Drawsphere() {
+        var mySphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        mySphere.transform.localScale = new Vector3(1,1,1);
+        mySphere.transform.position = new Vector3(0, 0, 0);
+    }
+
+    public void GenerateQR()
+    {
+        QR
+    }
+
+}
